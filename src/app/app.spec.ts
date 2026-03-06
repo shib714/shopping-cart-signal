e0c03866bef666fa20cccd3a496b164d42930310
shib7 <shib714@gmail.com>
2026-03-06 12:18:11 -0500

    updated to angular 21.2.1

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -17,7 +19,8 @@ describe('App', () => {
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
+    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shopping-cart-signal');
+    expect(compiled.textContent).toContain('Signal based Shopping Cart');
   });
 });
